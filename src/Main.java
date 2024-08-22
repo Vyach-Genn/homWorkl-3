c21ab7e8f942667f7be7cb911b2235a902e1546a
vachg <vachgen@yandex.ru>
2024-08-22 14:27:16 +0300

    Corrected

--- src/Main.java
+++ src/Main.java
@@ -34,16 +34,16 @@ public class Main {
         System.out.println("На каждого ученика рассчитано " + eachSheets + " листов бумаги.");
 
         // Задача №4
-        int controlTime = 16;
-        System.out.println("За 2 минуты машина произвела " + controlTime + " штук бутылок.");
-        controlTime = controlTime * 10;
-        System.out.println("За 20 минуты машина произвела " + controlTime + " штук бутылок.");
-        controlTime = controlTime * 72;
-        System.out.println("За сутки машина произвела " + controlTime + " штук бутылок.");
-        controlTime = controlTime * 3;
-        System.out.println("За 3 дня машина произвела " + controlTime + " штук бутылок.");
-        controlTime = controlTime * 10;
-        System.out.println("За месяц машина произвела " + controlTime + " штук бутылок.");
+        int controlTime2Minutes = 16; // за 2 минуты
+        System.out.println("За 2 минуты машина произвела " + controlTime2Minutes + " штук бутылок.");
+        int controlTime20Minutes = controlTime2Minutes * 10; // за 20 минут
+        System.out.println("За 20 минуты машина произвела " + controlTime20Minutes + " штук бутылок.");
+        int controlTimeDay = controlTime20Minutes * 72; // за 24 часа.
+        System.out.println("За сутки машина произвела " + controlTimeDay + " штук бутылок.");
+        int controlTime3Day = controlTimeDay * 3; // за 3 дня
+        System.out.println("За 3 дня машина произвела " + controlTime3Day + " штук бутылок.");
+        int controlTime30Day = controlTime3Day * 10; // за 30 дней
+        System.out.println("За месяц машина произвела " + controlTime30Day + " штук бутылок.");
 
         // Задача №5
         byte totalCans = 120;
@@ -54,14 +54,14 @@ public class Main {
                 + " банок белой краски и " + brownPaint + " банок коричневой краски.");
 
         // Задача №6
-        byte banana = 2;
+        byte banana = 5; // была, ошибка в колличестве бананов.
         short milk = 200;
         byte iceCream =2;
         byte egg = 4;
-        int weightBanana = banana * 80;
-        int weightMilk = milk / 100 * 105;
-        int weightIceCream = iceCream * 100;
-        int weightEgg = egg * 70;
+        int weightBanana = banana * 80; // вес 2 бананов
+        int weightMilk = milk / 100 * 105; // вес 200 ( 2 по 100 мг.) милилитров молока
+        int weightIceCream = iceCream * 100; // вес 2 мороженого
+        int weightEgg = egg * 70; // вес 4 яиц.
         int totalWweightice = weightBanana + weightMilk + weightIceCream + weightEgg;
         float totalWweighticeGram = (float) totalWweightice;
         float totalWweighticeKilogram = totalWweighticeGram / 1000;
@@ -69,23 +69,23 @@ public class Main {
                 + totalWweighticeKilogram + " килограмм.");
 
         // Задача №7
-        int twoHundredFifty = 7 * 1000 / 250;
-        int fiveHundred = 7 * 1000 / 500;
-        int averageDays = (twoHundredFifty + fiveHundred) / 2;
+        int days250Grams = 7 * 1000 / 250;
+        int days500Grams = 7 * 1000 / 500;
+        int averageDays = (days250Grams + days500Grams) / 2;
         System.out.println("Если спортсмен будет терять каждый день по 250 грамм, то похудеет на 7 кг за "
-                + twoHundredFifty + " дней, а если на 500 грамм , то за " + fiveHundred + " дней. Среднее же количество "
+                + days250Grams + " дней, а если на 500 грамм , то за " + days500Grams + " дней. Среднее же количество "
                 + averageDays + " день");
 
         // Задача №8
-        int masha = 67760;
+        int masha = 67760; // Зарплата до повышения
         int denis = 83690;
         int christina = 76230;
-        double mashaIncrease = masha + masha * 0.1;
-        double denisIncrease = denis + denis * 0.1;
-        double christinaIncrease = christina + christina * 0.1;
-        double yearMasha = mashaIncrease * 12 - masha * 12;
-        double yearDenis = denisIncrease * 12 - denis * 12;
-        double yearChristina = christinaIncrease * 12 - christina * 12;
+        int mashaIncrease = (int) (masha * 1.1); // Добавляем по 10%
+        int denisIncrease = (int) (denis * 1.1);
+        int christinaIncrease = (int) (christina * 1.1);
+        int yearMasha = mashaIncrease * 12 - masha * 12; // Вычесляе разницу годовых доходов
+        int yearDenis = denisIncrease * 12 - denis * 12;
+        int yearChristina = christinaIncrease * 12 - christina * 12;
         System.out.println("Маша теперь получает " + mashaIncrease + " рублей. Годовой доход вырос на "
                 + yearMasha + " рублей");
         System.out.println("Денис теперь получает " + denisIncrease + " рублей. Годовой доход вырос на "
